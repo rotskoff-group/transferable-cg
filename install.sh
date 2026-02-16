cbbf0f2f0d91c3f6b052397773db59d83a481566
abigailspark <abpark@stanford.edu>
2026-02-15 16:18:00 -0800

    Fixed installation and cleaned up dependencies

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-CUDA_VERSION=$(uv run python -c "import torch; print(torch.__version__.split('+')[-1])")
+CUDA_VERSION=$(uv run python -c "import torch; print('cpu' if not torch.cuda.is_available() else torch.__version__.split('+')[-1])")
 uv pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.0+${CUDA_VERSION}.html
